841dfc2d096a287b86a190f3cd613ec157a5d9fe
RT Sullivan (work computer) <atleast24.7@list.ru>
2017-12-15 19:29:34 +0800

    Finished with stores

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import 'font-awesome/css/font-awesome.min.css'
+import wilddog from 'wilddog'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './stores/index'
 
 Vue.config.productionTip = false
+
+const config = {
+  syncURL: 'https://wd8113259295dwfyda.wilddogio.com',
+  authDomain: 'wd8113259295dwfyda.wilddog.com'
+}
+wilddog.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
